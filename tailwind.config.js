03098b4ecf5cae4e3e9107bd162ac77ea2f770af
nicng123 <128271114+nicng123@users.noreply.github.com>
2025-09-28 04:30:54 +0000

    Project Beta 7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        xl: "1rem",
+        "2xl": "1.5rem",
+      },
+      colors: {
+        softBg: "#f7f8fa",
+        softPrimary: "#a2d2ff",
+        softAccent: "#ffc8dd",
+      },
+    },
   },
   plugins: [],
 }
